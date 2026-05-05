686715b0d7aac99b6d0671c22402e3b5a93ae06b
Roeseltransportes <amanda.cardoso@roeseltransportes.com.br>
2026-05-05 09:22:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://roesel-frontend.vercel.app",
-        "https://roesel-transportes.vercel.app",
-        "http://localhost:3000",
-        "http://localhost:3001",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
